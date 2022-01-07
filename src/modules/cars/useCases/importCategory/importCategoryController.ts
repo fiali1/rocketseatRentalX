2054dc2bafccc380968eb3080c4b17de35a14fee
Fiali1 <fiali1@hotmail.com>
2022-01-07 16:02:40 -0300

    docker implementation

---
+++ src/modules/cars/useCases/importCategory/importCategoryController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+class ImportCategoryController {
+  constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
+
+  handle(req: Request, res: Response): Response {
+    const { file } = req;
+
+    if (!file) {
+      throw new Error("File not found");
+    }
+
+    this.importCategoryUseCase.execute(file);
+
+    return res.send();
+  }
+}
+
+export { ImportCategoryController };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.get("/", (_req, res) => {
   return listCategoriesController.handle(_req, res);
@@ -11,6 +17,10 @@ categoriesRoutes.get("/", (_req, res) => {
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
